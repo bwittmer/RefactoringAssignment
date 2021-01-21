2f8f8766181b8fa3f7dc8e553b17aff5e0253fbf
Unknown <bwittmer@bsu.edu>
2021-01-21 13:31:58 -0500

    Using Extract Delegate refactoring

---
+++ src/InsuranceStrategyVeryHigh.java
@@ -0,0 +1,23 @@
+public class InsuranceStrategyVeryHigh {
+    private final InsuranceCalculator insuranceCalculator;
+
+    public InsuranceStrategyVeryHigh(InsuranceCalculator insuranceCalculator) {
+        this.insuranceCalculator = insuranceCalculator;
+    }
+
+    public double calculateInsuranceVeryHigh(double income) {
+        return (income - getAdjustment()) * getWeight() + getConstant();
+    }
+
+    public int getConstant() {
+        return 105600;
+    }
+
+    public double getWeight() {
+        return 0.02;
+    }
+
+    public int getAdjustment() {
+        return 60000;
+    }
+}
